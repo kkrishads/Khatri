aef4cc0bbbf776107232900189004655144cdaf8
gowri <gowri@alttab.com.au>
2016-07-14 18:38:47 +0530

    Updated

--- app/src/main/java/com/myinfos/supports/Services.java
+++ app/src/main/java/com/myinfos/supports/Services.java
@@ -4,5 +4,5 @@ package com.myinfos.supports;
  * Created by admin on 14/07/2016.
  */
 public class Services {
-    public static String url="http://172.21.4.194/contact/getContactList.php";
+    public static String url="http://toiletech.in/contact/getContactList.php";
 }
